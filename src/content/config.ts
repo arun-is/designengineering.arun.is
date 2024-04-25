63b71fc1df5a3b5807b7d2854c457286bd94e130
Arun Venkatesan <dallasarun@gmail.com>
2024-04-25 15:47:04 -0700

    dry up with components and content collections

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from "astro:content"
+
+const postsCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    description: z.string(),
+    date: z.date(),
+    url: z.string().url()
+  })
+})
+
+export const collections = {
+  posts: postsCollection
+}
---
+++ src/content/posts/the-origins-of-design-engineering.md
@@ -0,0 +1,9 @@
+---
+title: The origins of design engineering
+author: David Luhr
+date: 2024-02-26
+description:
+  Traces the origins and evolution of the design engineering role and discusses
+  how it has gained momentum in the industry over the past few years.
+url: https://luhr.co/blog/2024/02/26/the-origins-of-design-engineering/
+---
